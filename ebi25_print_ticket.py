378150dc10b52c06634350eb2f020f1726d91940
Daniel Rice <dlrice@ebi.ac.uk>
2022-06-30 16:57:30 +0100

    Testing.

--- ebi25_print_ticket.py
+++ ebi25_print_ticket.py
@@ -78,29 +78,29 @@ def failure(exception):
     )
 
 
-# @app.route("/print/<int:number>")
-# def print_number(number):
-#     app.logger.info('received request: {}'.format(number))
-#     if number >= 1e6:
-#         return failure('{} >= 1e6'.format(number))
-#     if number < 1e5:
-#         return failure('{} < 1e5'.format(number))
-#     number = '{:6d}'.format(number)
-#     try:
-#         html_path, pdf_path = generate_pdf(number)
-#         send_pdf_to_printer(pdf_path)
-#         clean_up(html_path, pdf_path)
-#     except Exception as e:
-#         print(e)
-#         return failure(e)
-#     else:
-#         return success()
+@app.route("/print/<int:number>")
+def print_number(number):
+    app.logger.info("received request: {}".format(number))
+    if number >= 1e6:
+        return failure("{} >= 1e6".format(number))
+    if number < 1e5:
+        return failure("{} < 1e5".format(number))
+    number = "{:6d}".format(number)
+    try:
+        html_path, pdf_path = generate_pdf(number)
+        send_pdf_to_printer(pdf_path)
+        clean_up(html_path, pdf_path)
+    except Exception as e:
+        print(e)
+        return failure(e)
+    else:
+        return success()
 
 
 def test():
     # pdf_path = generate_pdf('123456')
     # send_pdf_to_printer(pdf_path)
-    print("I'm laughing at you")
+    print("Just give up hahahaha")
 
 
 if __name__ == "__main__":
